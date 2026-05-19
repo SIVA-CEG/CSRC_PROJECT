11c2aceeeb2c041e6dc9460533257f318549ba5d
SIVA-CEG <siva13november2005@gmail.com>
2026-05-19 15:35:08 +0530

    Created initial React frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "20px",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+      }}
+    >
+      <button>Button 1</button>
+      <button>Button 2</button>
+      <button>Button 3</button>
+      <button>Button 4</button>
+      <button>Button 5</button>
+    </div>
+  );
+}
+
+export default App;
